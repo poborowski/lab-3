629efa11b08cc839b762204ba4971889477a2bea
poborowski <93825723+poborowski@users.noreply.github.com>
2021-12-27 20:26:56 +0100

    Add files via upload

---
+++ WebApplication2/Models/FormModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication2.Models
+{
+    public class FormModel
+    {
+        [Required]
+        [Display(Name = "Podaj imię")]
+
+        public string Name { get; set; }
+
+        [Display(Name = "Podaj ilość powtórzeń")]
+        [Range(1, 5)]
+        public int Repeat { get; set; }
+
+    }
+}
+
